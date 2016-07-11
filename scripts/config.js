014bf74a767a00000cccb25ae31b6ae1a096e7fe
Kiefer Hagin <kieferhagin@gmail.com>
2016-07-11 14:52:53 -0700

    deploy

---
+++ scripts/config.js
@@ -0,0 +1,5 @@
+angular.module('webScheduleConfig', [])
+
+.constant('apiConfig', {host:'https://stage-api.knockrentals.com/v1',facebook:{clientId:'964380833589174'},linkedin:{clientId:'75obqb93h535hz'}})
+
+;
